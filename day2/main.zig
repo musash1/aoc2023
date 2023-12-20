435e286cf363fdc3dea2feb8b3c65b8a4c4989fb
musash1 <luis.gjokaj@gmail.com>
2023-12-20 13:50:46 +0000

    day2 part1 zig finished

--- day2/main.zig
+++ day2/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const file = @embedFile("test.txt");
+    const file = @embedFile("text.txt");
 
     const returnP1 = try part1(file);
 
@@ -10,21 +10,42 @@ pub fn main() !void {
 
 fn part1(input: []const u8) !usize {
     const maxCubes = [3]u4{ 12, 13, 14 };
-    _ = maxCubes;
     const numbers = @Vector(10, i32);
     _ = numbers;
     var result: usize = 0;
-    _ = result;
     var lines = std.mem.tokenizeScalar(u8, input, '\n');
 
     while (lines.next()) |line| {
         var gameInput = std.mem.splitScalar(u8, line, ':');
-        var gameIdString = gameInput.next().?;
-        _ = gameIdString;
+        var gameString = gameInput.next().?;
+        var gameName = std.mem.splitScalar(u8, gameString, ' ');
+        var gameNameString = gameName.next().?;
+        _ = gameNameString;
+        var gameIdString = gameName.next().?;
+        var gameId = try std.fmt.parseInt(usize, gameIdString, 10);
         var hands = gameInput.next().?;
-        var singeGames = std.mem.tokenizeScalar(u8, hands, ';');
-        std.debug.print("{s}", .{singeGames.next().?});
-    }
+        var hand = std.mem.splitScalar(u8, hands, ';');
+        var impossible: bool = false;
+        while (hand.next()) |x| {
+            var cubes = std.mem.splitScalar(u8, x, ',');
+            while (cubes.next()) |y| {
+                var segment = std.mem.tokenizeScalar(u8, y, ' ');
+                const numberString = segment.next().?;
+                const colorString = segment.next().?;
+                const numberValue = try std.fmt.parseInt(usize, numberString, 10);
 
-    return 0;
+                if (std.mem.eql(u8, colorString, "red") and numberValue > maxCubes[0]) {
+                    impossible = true;
+                } else if (std.mem.eql(u8, colorString, "green") and numberValue > maxCubes[1]) {
+                    impossible = true;
+                } else if (std.mem.eql(u8, colorString, "blue") and numberValue > maxCubes[2]) {
+                    impossible = true;
+                }
+            }
+        }
+        if (!impossible) {
+            result += gameId;
+        }
+    }
+    return result;
 }
