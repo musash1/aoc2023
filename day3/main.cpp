5099a45b96aa24a0d695e5e3f9d3160683d54c03
musash1 <luis.gjokaj@gmail.com>
2023-12-23 00:52:50 +0000

    transformed into vector of strings

--- day3/main.cpp
+++ day3/main.cpp
@@ -1,14 +1,28 @@
 #include <fstream>
 #include <iostream>
+#include <cctype>
+#include <string>
+#include <vector>
 
 using std::cout, std::string;
 
 int part1() {
     string text;
     std::ifstream data("test.txt");
-
+    std::vector<string> textLines;
+    int x = 0;
+    
     while (getline(data, text)) {
-        cout << text << '\n';
+        textLines.push_back(text);
+        x++;
+    }
+    for (int i = 0; i < x; i++) {
+        char temp;
+        for (int j = 0; j < textLines.size(); j++) {
+            if (isdigit(textLines[i][j])) {
+                cout << textLines[i][j]; 
+            }
+        }
     }
 
     return 0;
