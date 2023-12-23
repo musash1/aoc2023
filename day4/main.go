8244245be9b046d38312b7151630e9dcb15f3b23
musash1 <luis.gjokaj@gmail.com>
2023-12-23 22:59:17 +0000

    i like go :D part2

--- day4/main.go
+++ day4/main.go
@@ -16,8 +16,10 @@ func main() {
     }
 
     p1 := part1(string(file))
+    p2 := part2(string(file))
 
     fmt.Println(p1)
+    fmt.Println(p2)
 
 }
 
@@ -25,7 +27,6 @@ func part1(file string) float64 {
     result := 0.0
     games := strings.Split(file, "\n") 
     for _, line := range games {
-       
         game := strings.Split(line, ":") 
         cards := strings.Split(game[len(game)-1], "|")
         luckyNums := strings.Split(cards[0], " ") 
@@ -49,6 +50,49 @@ func part1(file string) float64 {
     return result 
 }
 
-func part2(file string) float64 {
-    return 0.0
+func part2(file string) int {
+    result := 0
+    games := strings.Split(file, "\n") 
+    realGames := []string{}
+    nums := []int{}
+    for _, line := range games {
+        if line != "" {
+            realGames = append(realGames, line)
+        }
+    }
+    for line := range realGames {
+        line = line
+        nums = append(nums, 1) 
+    } 
+    for idx, line := range realGames {
+        j := 0
+        for j < nums[idx] {
+            game := strings.Split(line, ":") 
+            cards := strings.Split(game[len(game)-1], "|")
+            luckyNums := strings.Split(cards[0], " ") 
+            winningNums := strings.Split(cards[len(cards)-1], " ") 
+            matchNums := 0 
+
+            for _, num := range winningNums {
+                if slices.Contains(luckyNums, num) && num != "" {
+                    matchNums += 1
+                }
+            }
+            
+            i := 0 
+            if (matchNums > 0) {
+                for i < matchNums {
+                    nums[idx + i + 1] += 1
+                    i++
+                }
+            }
+            j++
+        }
+    }
+
+    for _, val := range nums {
+        result += val
+    }
+
+    return result 
 }
