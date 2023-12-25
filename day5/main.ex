b6d373c667bf9e96f8c2d24160915e6af5955e16
musash1 <luis.gjokaj@gmail.com>
2023-12-25 14:33:45 +0000

    10min of work :(

--- day5/main.ex
+++ day5/main.ex
@@ -1,1 +1,14 @@
-IO.puts "hello"
+defmodule Day5 do
+    def main do
+        {:ok, contents} = File.read("test.txt") 
+        part1(contents)
+    end
+
+    def part1(file) do
+        lines = String.split(file, "\n", trim: true)
+        lines2 = Enum.filter(lines, fn x -> Regex.match?(~r/\d+/, x) == true end)
+        IO.inspect lines2
+    end
+end
+
+Day5.main()
